b529aed0beee05939c789aa0f2d57420e64305df
Olzhas Kulzhambekov <exgamer@live.ru>
2021-03-02 16:07:11 +0600

    +

---
+++ src/V1/DataProcessor/DbDataHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Citizenzet\Laravel\Core\V1\DataProcessor;
+
+use Citizenzet\Php\Core\DataProcessor\DataHandler as Base;
+
+class DbDataHandler extends Base
+{
+    /**
+     * @return Query
+     */
+    function getQuery()
+    {
+        // TODO: Implement getQuery() method.
+    }
+}
